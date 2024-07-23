d19656ab22da243592d98e98fba3258295bc9d92
haruhi.s <haruhi.s@protonmail.com>
2024-07-23 19:21:13 +0800

    .

--- custom.el
+++ custom.el
@@ -7,7 +7,8 @@
    '("34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
-   '(yaml-mode vterm all-the-icons paredit pyim pyim-basedict slack slime slime-company syntax-subword telega vundo)))
+   '(minibuffer-line ytel emms highlight-thing yaml-mode vterm all-the-icons paredit pyim pyim-basedict slack slime slime-company syntax-subword telega vundo))
+ '(safe-local-variable-values '((buffer-file-coding-system . utf-8-unix))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- packages.el
+++ packages.el
@@ -29,6 +29,8 @@
 (package! slime-company)
 (package! paredit)
 (package! haskell-mode)
+(package! emms)
+(package! vterm)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
